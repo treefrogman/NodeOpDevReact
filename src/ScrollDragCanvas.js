5db159d236749834b109d8ef5d8a41a6e87827dd
Miles Cooley <treefrogman@gmail.com>
2022-06-05 15:03:48 -0700

    improve background noise performance on old chrome

--- src/ScrollDragCanvas.js
+++ src/ScrollDragCanvas.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from 'react';
 import useCorrectedDevicePixelRatio from "./utils/useCorrectedDevicePixelRatio";
+import noise from "./noise.svg";
 
 export default function ScrollDragCanvas(props) {
 	const { onPan, x, y, width, height, children, ...other } = props;
@@ -11,6 +12,7 @@ export default function ScrollDragCanvas(props) {
 	const x0 = (x || 0) - halfwidth;
 	const y0 = (y || 0) - halfheight;
 	const dpr = useCorrectedDevicePixelRatio();
+	const noiseScale = 500;
 	
 	function updateOffset(delta) {
 		// Multiply by -1 to invert the direction of the drag
@@ -48,21 +50,15 @@ export default function ScrollDragCanvas(props) {
 	}
 	
 	function onWheel(e) {
-		updateOffset({ x: -e.deltaX / 6, y: -e.deltaY / 6 });
+		updateOffset({ x: -e.deltaX / 2, y: -e.deltaY / 2 });
 	}
 	return (
 		<svg ref={rootRef} width={width} height={height+1} viewBox={[x0, y0, width, height+1].join(" ")} onPointerDown={onPointerDown} onPointerUp={onPointerUp} onPointerMove={onPointerMove} onWheel={onWheel} onAuxClick={onAuxClick} {...other}>
-			<filter id='noise'>
-				<feTurbulence baseFrequency=".5" />
-				<feColorMatrix type='matrix' values='
-					0 0 0 0 .25
-					0 0 0 0 .25
-					0 0 0 0 .25
-					-10 -10 -10 0 5
-				' />
-			</filter>
+			<pattern id="noise" patternUnits="userSpaceOnUse" width={noiseScale} height={noiseScale}>
+				<image href={noise} x="0" y="0" width={noiseScale} height={noiseScale} />
+			</pattern>
 			<rect className="ScrollDragCanvas-back" x={x0} y={y0} width={width} height={height} />
-			<rect ref={backRef} className="ScrollDragCanvas-handle" filter="url(#noise)" opacity=".15" x={x0} y={y0} width={width} height={height} />
+			<rect ref={backRef} className="ScrollDragCanvas-handle" fill="url(#noise)" x={x0} y={y0} width={width} height={height} />
 			{ children }
 		</svg>
 	);
