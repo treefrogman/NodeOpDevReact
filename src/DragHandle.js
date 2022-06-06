a6ec120e9a83edebb85863542c24acfd0eebe265
Miles Cooley <treefrogman@gmail.com>
2022-06-06 01:38:27 -0700

    factored out dragging

--- src/DragHandle.js
+++ src/DragHandle.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 
 export default function DragHandle(props) {
-	const { children, onDragStart, onDragMove, onDragEnd } = props;
+	const { children, onDragStart, onDragMove, x, y } = props;
+	const [startDragHandleOffset, setStartDragHandleOffset] = useState({ x: 0, y: 0 });
 	const [startDragCursorOffset, setStartDragCursorOffset] = useState({ x: 0, y: 0 });
 	const [drag, setDrag] = useState(false);
 	function onPointerDown(e) {
@@ -9,6 +10,7 @@ export default function DragHandle(props) {
 			// Left click
 			if (e.target === e.currentTarget) {
 				setDrag(true);
+				setStartDragHandleOffset({ x, y });
 				setStartDragCursorOffset({ x: e.screenX, y: e.screenY });
 				e.target.setPointerCapture(e.pointerId);
 				onDragStart();
@@ -18,7 +20,7 @@ export default function DragHandle(props) {
 	function onMouseMove(e) {
 		if (drag) {
 			// Left click
-			onDragMove({ x: startDragCursorOffset.x - e.screenX, y: startDragCursorOffset.y - e.screenY });
+			onDragMove({ x: startDragCursorOffset.x - e.screenX + startDragHandleOffset.x, y: startDragCursorOffset.y - e.screenY + startDragHandleOffset.y });
 		}
 	}
 	function onPointerUp(e) {
--- src/ScrollDragCanvas.js
+++ src/ScrollDragCanvas.js
@@ -1,40 +1,28 @@
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import useCorrectedDevicePixelRatio from "./utils/useCorrectedDevicePixelRatio";
 import BackgroundPattern from "./BackgroundPattern";
 import DragHandle from "./DragHandle";
 
 export default function ScrollDragCanvas(props) {
 	const { onPan, x, y, width, height, children, ...other } = props;
-	const backRef = useRef(null);
-	const rootRef = useRef(null);
-	const halfwidth = width / 2;
-	const halfheight = height / 2;
 	const [panning, setPanning] = useState(false);
-	const [startDragCanvasOffset, setStartDragCanvasOffset] = useState({ x: 0, y: 0 });
-	const x0 = (x || 0) - halfwidth;
-	const y0 = (y || 0) - halfheight;
+	const x0 = (x || 0) - width / 2;
+	const y0 = (y || 0) - height / 2;
 	const dpr = useCorrectedDevicePixelRatio();
 	const backgoundPattern = [
 		{ type: "hex", size: 370 },
 		{ type: "grid", size: 60 },
 	][1];
 
-	// function updateOffset(newOffset) {
-	// 	onPan({ x: startDragCanvasOffset.x + (startDragCursorOffset.x - newOffset.x) / dpr, y: startDragCanvasOffset.y + (startDragCursorOffset.y - newOffset.y) / dpr });
-	// }
-
 	function updateRelative(delta) {
 		onPan({ x: x - delta.x, y: y - delta.y });
 	}
 	
 	function onPointerDown(e) {
-		if (e.button === 2) {
-			// Right click
-			// contextmenu nyi
-		} else if (e.button === 1) {
+		if (e.button === 1) {
 			// Middle click
 			setPanning(true);
-			rootRef.current.requestPointerLock();
+			e.target.requestPointerLock();
 		}
 	}
 	function onMouseMove(e) {
@@ -56,19 +44,12 @@ export default function ScrollDragCanvas(props) {
 		updateRelative({ x: -e.deltaX / 2, y: -e.deltaY / 2 });
 	}
 	
-	function onDragStart() {
-		setStartDragCanvasOffset({x, y});
-	}
-	function onDragMove(drag) {
-		onPan({ x: startDragCanvasOffset.x + drag.x, y: startDragCanvasOffset.y + drag.y });
-	}
-	
 	return (
-		<svg ref={rootRef} width={width} height={height+1} viewBox={[x0, y0, width, height+1].join(" ")} onPointerDown={onPointerDown} onPointerUp={onPointerUp} onMouseMove={onMouseMove} onWheel={onWheel} onAuxClick={onAuxClick} {...other}>
+		<svg width={width} height={height+1} viewBox={[x0, y0, width, height+1].join(" ")} onPointerDown={onPointerDown} onPointerUp={onPointerUp} onMouseMove={onMouseMove} onWheel={onWheel} onAuxClick={onAuxClick} {...other}>
 			<BackgroundPattern pattern={backgoundPattern.type} size={backgoundPattern.size} />
 			<rect className="ScrollDragCanvas-back" x={x0} y={y0} width={width} height={height} />
-			<DragHandle {...{onDragStart, onDragMove }}>
-				<rect ref={backRef} className="ScrollDragCanvas-handle" fill={`url(#${backgoundPattern.type})`} x={x0} y={y0} width={width} height={height} />
+			<DragHandle onDragMove={onPan} x={x} y={y}>
+				<rect className="ScrollDragCanvas-handle" fill={`url(#${backgoundPattern.type})`} x={x0} y={y0} width={width} height={height} />
 			</DragHandle>
 			{ children }
 		</svg>
